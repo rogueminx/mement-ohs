b84d521bdc8a7b82f5ca254c3ec1d7c7d2e7ab60
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-27 11:21:17 -0700

    recipient saves to DB

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -4,6 +4,18 @@ class RecipientsController < ApplicationController
   end
 
   def create
-    @recipient = Recipient.new
+    @recipient = Recipient.new(recipient_params)
+    @recipient.user_id = current_user.id
+    if @recipient.save
+      redirect_to user_path(current_user)#change to new_recipient_memento_path
+    else
+      render :new
+    end
   end
+
+private
+  def recipient_params
+    params.require(:recipient).permit(:name, :email, :relation)
+  end
+
 end
