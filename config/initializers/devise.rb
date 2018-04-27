402bf848acca47ff439cbd2e0f3390840daee4d3
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-04-27 08:45:12 -0700

    permit login through devise with username and correctly display username in navbar.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '865e3ebee2264db88b6f9fa4f9b1ed584b2e1448639342f8d20696b38c70511f9f47b3c8fde5060f6511582a4054211929bdd6fedc3ba71f9823497018bbfea6'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -280,4 +280,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.authentication_keys = [ :login ]
 end
