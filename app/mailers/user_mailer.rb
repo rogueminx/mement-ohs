827206245a50667d4d6af4184e5fd9eb1c04203f
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-05-11 12:24:40 -0700

    add share memento email and associated views.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,12 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
      @user = user
      mail(to: @user.email, subject: 'Welcome to Mement-oh')
-   end
+  end
+
+  def share_memento_email(user, recipient, url)
+     @user = user
+     @recipient = recipient
+     @url = url
+     mail(to: @recipient.email, subject: "#{@user.name} wants to share a Mement-oh with you.")
+  end
 end
