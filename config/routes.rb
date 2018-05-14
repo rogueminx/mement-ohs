69cc08389680afd5629632bbe041eed6b38da6ad
Anna-Marie Cooper <annamarie@rogueminx.com>
2018-05-13 20:40:19 -0700

    removed bug in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'mementos', to: 'mementos#all', as: :mementos
-  get 'send_mail', to: 'UserMailer#share_memento_email(user,recipient, url)', as: :send_memento_mail
+  # get 'send_mail', to: 'UserMailer#share_memento_email(user, recipient, url)', as: :send_memento_mail
 
   resources :recipients do
     resources :mementos
